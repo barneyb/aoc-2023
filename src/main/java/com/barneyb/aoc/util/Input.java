0aa5e854e23ccea52a102ee4288bc1f3baf8d6f0
Barney Boisvert <bboisvert@gmail.com>
2023-11-15 08:36:29 -0800

    move year/day parsing from Solve to Input

--- src/main/java/com/barneyb/aoc/util/Input.java
+++ src/main/java/com/barneyb/aoc/util/Input.java
@@ -5,6 +5,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.stream.Stream;
@@ -12,8 +15,53 @@ import java.util.stream.StreamSupport;
 
 public final class Input implements Iterable<String> {
 
+    private static final int FIRST_YEAR = 2015;
+    private static final int FIRST_DAY = 1;
+    private static final int LAST_DAY = 25;
+
     public static Input of(int year, int day) {
         return new Input(year, day);
+    }
+
+    /**
+     * I interrogate the passed Class's packages' names to identify the year and
+     * day it solves, and return the input for that day.
+     *
+     * @throws NumberFormatException    if the package names don't contain valid
+     *                                  year/day numeric strings.
+     * @throws IllegalArgumentException if the year/day is not valid
+     */
+    public static Input of(Class<?> clazz) {
+        String[] parts = clazz.getPackageName()
+                .split("\\.");
+        int year = Integer.parseInt(parts[parts.length - 2].substring(3));
+        int day = Integer.parseInt(parts[parts.length - 1].substring(3));
+        validateDay(year, day);
+        return Input.of(year, day);
+    }
+
+    private static void validateDay(int year, int day) {
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/New_York"));
+        int maxYear = year == now.getYear() && Month.DECEMBER == now.getMonth()
+                ? now.getYear()
+                : now.getYear() - 1;
+        if (year < FIRST_YEAR || year > maxYear) {
+            throw new IllegalArgumentException(String.format(
+                    "Year must be %s-%s",
+                    FIRST_YEAR,
+                    maxYear));
+        }
+        int maxDay = LAST_DAY;
+        if (year == now.getYear()) {
+            // already know it's december
+            maxDay = Math.min(maxDay, now.getDayOfMonth());
+        }
+        if (day < FIRST_DAY || day > maxDay) {
+            throw new IllegalArgumentException(String.format(
+                    "Day must be %s-%s",
+                    FIRST_DAY,
+                    maxDay));
+        }
     }
 
     /**
--- src/main/java/com/barneyb/aoc/util/Solve.java
+++ src/main/java/com/barneyb/aoc/util/Solve.java
@@ -4,9 +4,6 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryUsage;
-import java.time.Month;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,55 +15,14 @@ import java.util.function.Supplier;
 
 abstract class Solve<Model> {
 
-    private static final int FIRST_YEAR = 2015;
-    private static final int FIRST_DAY = 1;
-    private static final int LAST_DAY = 25;
-
-    private final int year;
-    private final int day;
-
-    Solve() {
-        String[] parts = getClass().getPackageName()
-                .split("\\.");
-        int year = Integer.parseInt(parts[parts.length - 2].substring(3));
-        int day = Integer.parseInt(parts[parts.length - 1].substring(3));
-        validateDay(year, day);
-        this.year = year;
-        this.day = day;
-    }
-
-    private void validateDay(int year, int day) {
-        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/New_York"));
-        int maxYear = year == now.getYear() && Month.DECEMBER == now.getMonth()
-                ? now.getYear()
-                : now.getYear() - 1;
-        if (year < FIRST_YEAR || year > maxYear) {
-            throw new IllegalArgumentException(String.format(
-                    "Year must be %s-%s",
-                    FIRST_YEAR,
-                    maxYear));
-        }
-        int maxDay = LAST_DAY;
-        if (year == now.getYear()) {
-            // already know it's december
-            maxDay = Math.min(maxDay, now.getDayOfMonth());
-        }
-        if (day < FIRST_DAY || day > maxDay) {
-            throw new IllegalArgumentException(String.format(
-                    "Day must be %s-%s",
-                    FIRST_DAY,
-                    maxDay));
-        }
-    }
-
     protected abstract Model buildModel(Input input);
 
     abstract void solve(Model model,
                         Consumer<Info<?>> doneWithPart);
 
     record Info<T>(T result,
-                             long nanos,
-                             Mem mem) {
+                   long nanos,
+                   Mem mem) {
 
         Info(T result) {
             this(result, 0, Mem.empty());
@@ -136,7 +92,7 @@ abstract class Solve<Model> {
     }
 
     Input getInput() {
-        return Input.of(year, day);
+        return Input.of(getClass());
     }
 
     private String part(int i) {
