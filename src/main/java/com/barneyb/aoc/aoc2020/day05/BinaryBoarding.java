95cfe13a56946e31102c0b3629cea1d619b03be2
Barney Boisvert <bboisvert@gmail.com>
2023-11-17 08:14:25 -0800

    2020 day 5 part two

--- src/main/java/com/barneyb/aoc/aoc2020/day05/BinaryBoarding.java
+++ src/main/java/com/barneyb/aoc/aoc2020/day05/BinaryBoarding.java
@@ -1,11 +1,11 @@
 package com.barneyb.aoc.aoc2020.day05;
 
 import com.barneyb.aoc.util.Input;
-import com.barneyb.aoc.util.SolvePartOne;
+import com.barneyb.aoc.util.SolveEachPart;
 
 import java.util.List;
 
-public class BinaryBoarding extends SolvePartOne<List<BoardingPass>, Integer> {
+public class BinaryBoarding extends SolveEachPart<List<BoardingPass>, Integer, Integer> {
 
     public static void main(String[] args) {
         new BinaryBoarding().solveAndPrint();
@@ -24,6 +24,18 @@ public class BinaryBoarding extends SolvePartOne<List<BoardingPass>, Integer> {
                 .mapToInt(BoardingPass::seatId)
                 .max()
                 .orElseThrow();
+    }
+
+    @Override
+    protected Integer solvePartTwo(List<BoardingPass> boardingPasses) {
+        int[] ids = boardingPasses.stream()
+                .mapToInt(BoardingPass::seatId)
+                .sorted()
+                .toArray();
+        for (int i = ids.length - 2; i >= 0; i--) {
+            if (ids[i] + 1 != ids[i + 1]) return ids[i] + 1;
+        }
+        throw new RuntimeException("No empty seat found");
     }
 
 }
