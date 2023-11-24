d1246d2c2899a9af01cbecd525876191fee0d6f6
Barney Boisvert <bboisvert@gmail.com>
2023-11-24 15:53:32 -0800

    give a little solver output

--- python/aoc2015/day01/not_quite_lisp.py
+++ python/aoc2015/day01/not_quite_lisp.py
@@ -1,6 +1,8 @@
-def both_parts(input):
+from util import aoc
+
+
+def part_one(input):
     floor = 0
-    pos = -1
     i = 0
     for c in input:
         i += 1
@@ -10,12 +12,27 @@ def both_parts(input):
             floor -= 1
         else:
             raise RuntimeError("Unknown '" + c + "'")
-        if floor == -1 and pos == -1:
-            pos = i
-    return floor, pos
+    return floor
+
+
+def part_two(input):
+    floor = 0
+    i = 0
+    for c in input:
+        i += 1
+        if c == '(':
+            floor += 1
+        elif c == ')':
+            floor -= 1
+        else:
+            raise RuntimeError("Unknown '" + c + "'")
+        if floor == -1:
+            return i
+    raise RuntimeError("Never entered basement")
 
 
 if __name__ == "__main__":
-    from aocd import get_data
-
-    print(both_parts(get_data(year=2015, day=1)))
+    aoc.solve(__file__,
+              None,
+              part_one,
+              part_two)
