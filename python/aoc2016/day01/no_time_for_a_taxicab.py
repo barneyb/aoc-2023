cb4af9322e0da980d528aaffc7da370f0583a855
Barney Boisvert <bboisvert@gmail.com>
2023-11-25 15:00:13 -0800

    2016 day 1 part two

--- python/aoc2016/day01/no_time_for_a_taxicab.py
+++ python/aoc2016/day01/no_time_for_a_taxicab.py
@@ -5,29 +5,43 @@ def parse(input):
     return [(s[0], int(s[1:])) for s in input.split(", ")]
 
 
-def part_one(model):
-    h = 0
-    x = 0
-    y = 0
-    for (d, n) in model:
-        if d == 'R':
-            h += 1
-        else:
-            h -= 1
-        match (h + 4) % 4:
-            case 0:
-                y -= n
-            case 1:
-                x += n
-            case 2:
-                y += n
-            case 3:
-                x -= n
+def part_one(path):
+    x, y = 0, 0
+    for x, y in walk(path):
+        pass
     return abs(x) + abs(y)
 
 
-def part_two(model):
-    return None
+def walk(path):
+    """Generator for all points along the path, which must be a sequence of
+    tuples. Each tuple's first item is either 'R' or 'L', indicating which way
+    to turn, and the second is how many steps to take after turning. The first
+    point at the origin is not yielded (you're already there).
+    """
+    h, x, y = 0, 0, 0
+    for d, n in path:
+        h = h + 1 if d == 'R' else h - 1
+        h %= 4
+        for _ in range(n):
+            match h:
+                case 0:
+                    y -= 1
+                case 1:
+                    x += 1
+                case 2:
+                    y += 1
+                case 3:
+                    x -= 1
+            yield x, y
+
+
+def part_two(path):
+    points = set()
+    for p in walk(path):
+        if p in points:
+            x, y = p
+            return abs(x) + abs(y)
+        points.add(p)
 
 
 if __name__ == "__main__":
--- python/aoc2016/day01/no_time_for_a_taxicab_test.py
+++ python/aoc2016/day01/no_time_for_a_taxicab_test.py
@@ -1,11 +1,16 @@
 from no_time_for_a_taxicab import *
 
-MODEL_ONE = [('R', 2), ('L', 3)]
+PATH_ONE = [('R', 2), ('L', 3)]
 
 
 def test_parse():
-    assert MODEL_ONE == parse("R2, L3")
+    assert PATH_ONE == parse("R2, L3")
 
 
 def test_part_one():
-    assert 5 == part_one(MODEL_ONE)
+    assert 5 == part_one(PATH_ONE)
+
+
+def test_part_two():
+    path = parse("R8, R4, R4, R8")
+    assert 4 == part_two(path)
