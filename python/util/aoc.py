d0eeb559573eec33b4ab0a0f4dc76a3a2bf900bd
Barney Boisvert <bboisvert@gmail.com>
2023-11-26 12:20:18 -0800

    split the puzzle utils from the timing utils

--- python/util/aoc.py
+++ python/util/aoc.py
@@ -1,13 +1,12 @@
 from os import path
 from re import split
-from time import perf_counter_ns
 
-NANOS_PER_MILLISECOND = 1_000_000
-
-NANOS_PER_SEC = 1_000_000_000
+from .perf import format_ns, timed_ns
 
 
 def get_input(file):
+    # This import checks the token file _during initialization_, so only import
+    # when it's first used.
     from aocd import get_data
 
     parts = split("[^0-9]+", path.dirname(file))
@@ -16,28 +15,15 @@ def get_input(file):
     return get_data(year=y, day=d)
 
 
-def timed_ns(work):
-    start = perf_counter_ns()
-    result = work()
-    end = perf_counter_ns()
-    return result, end - start
-
-
 def solve(file, parse, *parts):
     (input, ns) = timed_ns(lambda: get_input(file))
-    print(f"Read   ({duration_ns(ns)}): {len(input)} chars")
+    print(f"Read   ({format_ns(ns)}): {len(input)} chars")
     for i, part in enumerate(parts):
         if parse is None:
             model = input
         else:
             (model, ns) = timed_ns(lambda: parse(input))
-            if i == 0:
-                print(f"Parse  ({duration_ns(ns)})")
+            if i == 0 and model != input:
+                print(f"Parse  ({format_ns(ns)})")
         (answer, ns) = timed_ns(lambda: part(model))
-        print(f"Part {i + 1} ({duration_ns(ns)}): {answer if answer else '-'}")
-
-
-def duration_ns(nanos):
-    if nanos > NANOS_PER_SEC:
-        return f"{nanos / NANOS_PER_SEC :>7,.2f} sec"
-    return f"{nanos / NANOS_PER_MILLISECOND :>8,.2f} ms"
+        print(f"Part {i + 1} ({format_ns(ns)}): {answer if answer else '-'}")
