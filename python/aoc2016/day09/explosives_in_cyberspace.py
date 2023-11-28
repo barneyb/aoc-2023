c23a72b7de95128378e96b44dde17e48a14ccb1f
Barney Boisvert <bboisvert@gmail.com>
2023-11-27 20:42:31 -0800

    2016 day 9 part two

--- python/aoc2016/day09/explosives_in_cyberspace.py
+++ python/aoc2016/day09/explosives_in_cyberspace.py
@@ -22,7 +22,26 @@ def part_one(input):
     return decomp_len
 
 
+def part_two(input):
+    comp_len = len(input)
+    decomp_len = 0
+    pos = 0
+    while pos < comp_len:
+        m = RE_MARKER.search(input, pos)
+        if m is None:
+            decomp_len += comp_len - pos
+            break
+        decomp_len += m.start() - pos
+        l = int(m.group(1))
+        pos = m.end() + l
+        l = part_two(input[m.end():m.end() + l])
+        n = int(m.group(2))
+        decomp_len += l * n
+    return decomp_len
+
+
 if __name__ == "__main__":
     aoc.solve(__file__,
               None,
-              part_one)
+              part_one,
+              part_two)
--- python/aoc2016/day09/explosives_in_cyberspace_test.py
+++ python/aoc2016/day09/explosives_in_cyberspace_test.py
@@ -1,25 +1,41 @@
 from explosives_in_cyberspace import *
 
 
-def test_example_1():
+def test_example_1_1():
     assert part_one("ADVENT") == 6
 
 
-def test_example_2():
+def test_example_1_2():
     assert part_one("A(1x5)BC") == 7
 
 
-def test_example_3():
+def test_example_1_3():
     assert part_one("(3x3)XYZ") == 9
 
 
-def test_example_4():
+def test_example_1_4():
     assert part_one("A(2x2)BCD(2x2)EFG") == 11
 
 
-def test_example_5():
+def test_example_1_5():
     assert part_one("(6x1)(1x3)A") == 6
 
 
-def test_example_6():
+def test_example_1_6():
     assert part_one("X(8x2)(3x3)ABCY") == 18
+
+
+def test_example_2_1():
+    assert part_two("(3x3)XYZ") == len("XYZXYZXYZ")
+
+
+def test_example_2_2():
+    assert part_two("X(8x2)(3x3)ABCY") == len("XABCABCABCABCABCABCY")
+
+
+def test_example_2_3():
+    assert part_two("(27x12)(20x12)(13x14)(7x10)(1x12)A") == 241920
+
+
+def test_example_2_4():
+    assert part_two("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN") == 445
