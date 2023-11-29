f9d745a3bc7a828de127e83a0ed877fbdec65e23
Barney Boisvert <bboisvert@gmail.com>
2023-11-28 19:17:00 -0800

    fold the parts together

--- python/aoc2022/day01/calorie_counting.py
+++ python/aoc2022/day01/calorie_counting.py
@@ -1,42 +1,39 @@
 from util import aoc
 
 
-def part_one(input):
-    best = 0
-    curr = 0
-    for line in input.splitlines():
-        if line == "":
-            best = max(curr, best)
-            curr = 0
-        else:
-            curr += int(line)
-    return max(curr, best)
-
-
-def part_two(input):
-    def doit(a, b, c, d):
-        if d < a:
-            return a, b, c
-        if d < b:
-            return d, b, c
-        if d < c:
-            return b, d, c
-        return b, c, d
-
+def parse(input):
     a, b, c = 0, 0, 0
     curr = 0
     for line in input.splitlines():
         if line == "":
-            a, b, c = doit(a, b, c, curr)
+            a, b, c = largest_three(a, b, c, curr)
             curr = 0
         else:
             curr += int(line)
-    a, b, c = doit(a, b, c, curr)
-    return a + b + c
+    a, b, c = largest_three(a, b, c, curr)
+    return a, b, c
+
+
+def largest_three(a, b, c, d):
+    if d < b:
+        if d < a:
+            return a, b, c
+        return d, b, c
+    if d < c:
+        return b, d, c
+    return b, c, d
+
+
+def part_one(three):
+    return three[2]
+
+
+def part_two(three):
+    return sum(three)
 
 
 if __name__ == "__main__":
     aoc.solve(__file__,
-              None,
+              parse,
               part_one,
               part_two)
--- python/aoc2022/day01/calorie_counting_test.py
+++ python/aoc2022/day01/calorie_counting_test.py
@@ -16,10 +16,16 @@ EXAMPLE_ONE = """1000
 10000
 """
 
+MODEL_ONE = (10_000, 11_000, 24_000)
+
+
+def test_parse():
+    assert parse(EXAMPLE_ONE) == MODEL_ONE
+
 
 def test_part_one():
-    assert part_one(EXAMPLE_ONE) == 24_000
+    assert part_one(MODEL_ONE) == 24_000
 
 
 def test_part_two():
-    assert part_two(EXAMPLE_ONE) == 45_000
+    assert part_two(MODEL_ONE) == 45_000
