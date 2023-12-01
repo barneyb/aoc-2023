9f46b6ace13a5254150aac58119d3f2d6af00725
Barney Boisvert <bboisvert@gmail.com>
2023-12-01 13:59:17 -0800

    use a dict for the digit names

--- python/aoc2023/day01/trebuchet.py
+++ python/aoc2023/day01/trebuchet.py
@@ -2,6 +2,17 @@ import re
 
 from util import aoc
 
+NAMES = {"one"  : 1,
+         "two"  : 2,
+         "three": 3,
+         "four" : 4,
+         "five" : 5,
+         "six"  : 6,
+         "seven": 7,
+         "eight": 8,
+         "nine" : 9,
+         }
+
 
 def part_one(input):
     return either_part(
@@ -31,26 +42,7 @@ def part_two(input):
 def parse_word_digit(s):
     if len(s) == 1:
         return int(s)
-    match s:
-        case "one":
-            return 1
-        case "two":
-            return 2
-        case "three":
-            return 3
-        case "four":
-            return 4
-        case "five":
-            return 5
-        case "six":
-            return 6
-        case "seven":
-            return 7
-        case "eight":
-            return 8
-        case "nine":
-            return 9
-    raise RuntimeError(f"can't parse digit '{s}'")
+    return NAMES[s]
 
 
 if __name__ == "__main__":
