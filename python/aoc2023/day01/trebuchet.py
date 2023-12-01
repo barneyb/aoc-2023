a121e1a2c176e89d4dc1174814f6b310d5a16bd4
Barney Boisvert <bboisvert@gmail.com>
2023-11-30 22:12:07 -0800

    2023 day 1 part two

--- python/aoc2023/day01/trebuchet.py
+++ python/aoc2023/day01/trebuchet.py
@@ -2,24 +2,61 @@ import re
 
 from util import aoc
 
-RE_FIRST = re.compile(r"\D*(\d)")
-RE_LAST = re.compile(r"(\d)\D*$")
 
-
-def parse(input):
+def part_one(input):
+    re_first = re.compile(r"\D*(\d)")
+    re_last = re.compile(r"(\d)\D*$")
     nums = []
     for line in input.splitlines():
-        f = RE_FIRST.match(line)
-        l = RE_LAST.search(line)
+        f = re_first.match(line)
+        l = re_last.search(line)
         nums.append(f.group(1) + l.group(1))
-    return nums
+    return sum(int(n) for n in nums)
 
 
-def part_one(model):
-    return sum(int(n) for n in model)
+def part_two(input):
+    re_digit = re.compile(r"\d|one|two|three|four|five|six|seven|eight|nine")
+    model = []
+    for line in input.splitlines():
+        digits = []
+        i, l = 0, len(line)
+        while i < l:
+            m = re_digit.search(line, i)
+            if not m:
+                break
+            digits.append(parse_digit(m.group()))
+            i += 1
+        model.append(digits[0] * 10 + digits[-1])
+    return sum(model)
+
+
+def parse_digit(s):
+    if len(s) == 1:
+        return int(s)
+    match s:
+        case "one":
+            return 1
+        case "two":
+            return 2
+        case "three":
+            return 3
+        case "four":
+            return 4
+        case "five":
+            return 5
+        case "six":
+            return 6
+        case "seven":
+            return 7
+        case "eight":
+            return 8
+        case "nine":
+            return 9
+    raise RuntimeError(f"can't parse digit '{s}'")
 
 
 if __name__ == "__main__":
     aoc.solve(__file__,
-              parse,
-              part_one)
+              None,
+              part_one,
+              part_two)  # not 55330
--- python/aoc2023/day01/trebuchet_test.py
+++ python/aoc2023/day01/trebuchet_test.py
@@ -1,17 +1,28 @@
 from trebuchet import *
 
-EXAMPLE = """1abc2
+EXAMPLE_ONE = """1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet
 """
 
-MODEL = ["12", "38", "15", "77"]
-
-
-def test_parse():
-    assert parse(EXAMPLE) == MODEL
+EXAMPLE_TWO = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"""
 
 
 def test_example_one():
-    assert part_one(MODEL) == 142
+    assert part_one(EXAMPLE_ONE) == 142
+
+
+def test_example_two():
+    assert part_two(EXAMPLE_TWO) == 281
+
+
+def test_overlap():
+    assert part_two("twone") == 21
