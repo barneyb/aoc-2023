54e9366634dc9b35f3fa52d570dd51ab8e3be34e
Barney Boisvert <bboisvert@gmail.com>
2023-12-02 16:24:38 -0800

    2017 day 1 part two

--- python/aoc2017/day01/inverse_captcha.py
+++ python/aoc2017/day01/inverse_captcha.py
@@ -11,9 +11,20 @@ def part_one(s):
     return sum
 
 
+def part_two(s):
+    sum = 0
+    l = len(s)
+    half = l // 2
+    for i, c in enumerate(s):
+        if c == s[(i + half) % l]:
+            sum += int(c)
+    return sum
+
+
 if __name__ == "__main__":
     aoc.solve(
         __file__,
         None,
         part_one,
+        part_two,
     )
--- python/aoc2017/day01/inverse_captcha_test.py
+++ python/aoc2017/day01/inverse_captcha_test.py
@@ -6,3 +6,11 @@ def test_examples_1():
     assert part_one("1111") == 4
     assert part_one("1234") == 0
     assert part_one("91212129") == 9
+
+
+def test_examples_2():
+    assert part_two("1212") == 6
+    assert part_two("1221") == 0
+    assert part_two("123425") == 4
+    assert part_two("123123") == 12
+    assert part_two("12131415") == 4
