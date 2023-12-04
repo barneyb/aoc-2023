2814292e590164fdea2886771e1bbd65dbcaa93b
Barney Boisvert <bboisvert@gmail.com>
2023-12-03 19:13:49 -0800

    refactor w/ dynamic dimensions

--- python/aoc2020/day17/conway_cubes.py
+++ python/aoc2020/day17/conway_cubes.py
@@ -4,69 +4,53 @@ from util import aoc
 def parse(input):
     points = set()
     for y, line in enumerate(input.splitlines()):
-        for x, c in enumerate(line):
-            if c == "#":
-                points.add((x, y, 0))  # arbitrarily choosing z=0
+        for p in ((x, y) for x, c in enumerate(line) if c == "#"):
+            points.add(p)
     return points
 
 
-def neighbors(x, y, z):
-    return [
-        (x - 1, y - 1, z - 1),
-        (x, y - 1, z - 1),
-        (x + 1, y - 1, z - 1),
-        (x + 1, y, z - 1),
-        (x + 1, y + 1, z - 1),
-        (x, y + 1, z - 1),
-        (x - 1, y + 1, z - 1),
-        (x - 1, y, z - 1),
-        (x, y, z - 1),
-        (x - 1, y - 1, z),
-        (x, y - 1, z),
-        (x + 1, y - 1, z),
-        (x + 1, y, z),
-        (x + 1, y + 1, z),
-        (x, y + 1, z),
-        (x - 1, y + 1, z),
-        (x - 1, y, z),
-        (x - 1, y - 1, z + 1),
-        (x, y - 1, z + 1),
-        (x + 1, y - 1, z + 1),
-        (x + 1, y, z + 1),
-        (x + 1, y + 1, z + 1),
-        (x, y + 1, z + 1),
-        (x - 1, y + 1, z + 1),
-        (x - 1, y, z + 1),
-        (x, y, z + 1),
-    ]
+def cartesian_product(ranges):
+    def helper(basis, rs):
+        this_range, *others = rs
+        if len(others) == 0:
+            for d in this_range:
+                yield *basis, d
+        else:
+            for d in this_range:
+                yield from helper((*basis, d), others)
+
+    yield from helper((), ranges)
+
+
+def neighbors(p):
+    def helper():
+        yield from cartesian_product([range(d - 1, d + 2) for d in p])
+
+    return filter(lambda n: n != p, helper())  # sorta silly :)
 
 
 def tick(points):
-    xr = yr = zr = 0, 0
+    mins = maxes = [d for d in next(points.__iter__())]
     for p in points:
-        x, y, z = p
-        xr = min(xr[0], x - 1), max(xr[1], x + 2)
-        yr = min(yr[0], y - 1), max(yr[1], y + 2)
-        zr = min(zr[0], z - 1), max(zr[1], z + 2)
+        mins = [min(a, b) for a, b in zip(mins, p)]
+        maxes = [max(a, b) for a, b in zip(maxes, p)]
 
     state = set()
-    for x in range(*xr):
-        for y in range(*yr):
-            for z in range(*zr):
-                active_neighbors = sum(1 for n in neighbors(x, y, z) if n in points)
-                if active_neighbors == 3:
-                    state.add((x, y, z))
-                elif active_neighbors == 2:
-                    p = (x, y, z)
-                    if p in points:
-                        state.add(p)
+    for p in cartesian_product([range(mn - 1, mx + 2) for mn, mx in zip(mins, maxes)]):
+        active_neighbors = sum(1 for n in neighbors(p) if n in points)
+        if active_neighbors == 3:
+            state.add(p)
+        elif active_neighbors == 2:
+            if p in points:
+                state.add(p)
     return state
 
 
 def part_one(model, cycles=6):
+    state = set((*p, 0) for p in model)
     for _ in range(cycles):
-        model = tick(model)
-    return len(model)
+        state = tick(state)
+    return len(state)
 
 
 if __name__ == "__main__":
