d5641944e1017b9cb3ab1b1e1d16b9a68680551a
Barney Boisvert <bboisvert@gmail.com>
2023-12-09 14:35:56 -0800

    2016 day 16 part one

---
+++ python/aoc2016/day16/dragon_checksum.py
@@ -0,0 +1,41 @@
+from util import aoc
+
+
+def gen(a):
+    sb = [a, "0"]
+    for c in reversed(a):
+        sb.append("1" if c == "0" else "0")
+    return "".join(sb)
+
+
+def chksum(data):
+    if len(data) % 2 == 1:
+        return data
+    sb = []
+    for i in range(0, len(data), 2):
+        sb.append("1" if data[i] == data[i + 1] else "0")
+    return chksum("".join(sb))
+
+
+def fill_and_chksum(seed, size):
+    data = seed
+    while len(data) < size:
+        data = gen(data)
+    return chksum(data[:size])
+
+
+def part_one(seed):
+    return fill_and_chksum(seed, 272)
+
+
+# def part_two(model):
+#     return len(model)
+
+
+if __name__ == "__main__":
+    aoc.solve(
+        __file__,
+        None,
+        part_one,
+        # part_two,
+    )
---
+++ python/aoc2016/day16/dragon_checksum_test.py
@@ -0,0 +1,21 @@
+from .dragon_checksum import *
+
+
+def test_gen():
+    assert gen("1") == "100"
+    assert gen("0") == "001"
+    assert gen("11111") == "11111000000"
+    assert gen("111100001010") == "1111000010100101011110000"
+
+
+def test_chksum():
+    assert chksum("110010110100") == "100"
+    assert chksum("10000011110010000111") == "01100"
+
+
+def test_example_one():
+    assert fill_and_chksum("10000", 20) == "01100"
+
+
+# def test_example_two():
+#     assert part_two(MODEL) == 1_234
