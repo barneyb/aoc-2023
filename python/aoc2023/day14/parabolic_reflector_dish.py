fd18adf8e61fe28b16a8f2b1debf66400a3146da
Barney Boisvert <bboisvert@gmail.com>
2023-12-16 11:32:58 -0800

    2023 day 14 part two

--- python/aoc2023/day14/parabolic_reflector_dish.py
+++ python/aoc2023/day14/parabolic_reflector_dish.py
@@ -11,15 +11,9 @@ def parse(input):
     return len(lines[0]), len(lines), rocks
 
 
-def unparse(model):
-    w, h, rocks = model
-    sb = []
-    for y in range(h):
-        for x in range(w):
-            p = (x, y)
-            sb.append(rocks[p] if p in rocks else ".")
-        sb.append("\n")
-    return "".join(sb)
+def weight_on_north_support(model):
+    _, h, rocks = model
+    return sum(h - y for (_, y), r in rocks.items() if r == "O")
 
 
 def part_one(model):
@@ -32,11 +26,58 @@ def part_one(model):
         while y > 0 and (x, y - 1) not in rocks:
             y -= 1
         rocks[(x, y)] = r
-    return sum(h - y for (_, y), r in rocks.items() if r == "O")
+    return weight_on_north_support((w, h, rocks))
 
 
-# def part_two(model):
-#     return len(model)
+_spin = [
+    (lambda p: p[1], lambda p: (p[0], p[1] - 1)),
+    (lambda p: p[0], lambda p: (p[0] - 1, p[1])),
+    (lambda p: -p[1], lambda p: (p[0], p[1] + 1)),
+    (lambda p: -p[0], lambda p: (p[0] + 1, p[1])),
+]
+
+
+def in_bounds(p, w, h):
+    x, y = p
+    return 0 <= x < w and 0 <= y < h
+
+
+def spin_cycle(model):
+    w, h, rocks = model
+    for key, delta in _spin:
+        curr = rocks
+        rocks = {}
+        for p in sorted(curr.keys(), key=key):
+            r = curr[p]
+            if r == "#":
+                rocks[p] = r
+                continue
+            while True:
+                candidate = delta(p)
+                if in_bounds(candidate, w, h) and candidate not in rocks:
+                    p = candidate
+                else:
+                    break
+            rocks[p] = r
+    return w, h, rocks
+
+
+def spin_cycles(model, total_cycles):
+    history = {}
+    for i in range(total_cycles):
+        s = frozenset(model[2])
+        if s in history:
+            cycle_len = i - history[s]
+            full_cycles = (total_cycles - i) // cycle_len
+            to_align = total_cycles - full_cycles * cycle_len - i
+            return spin_cycles(model, to_align)
+        history[s] = i
+        model = spin_cycle(model)
+    return model
+
+
+def part_two(model):
+    return weight_on_north_support(spin_cycles(model, 1_000_000_000))
 
 
 if __name__ == "__main__":
@@ -44,5 +85,5 @@ if __name__ == "__main__":
         __file__,
         parse,
         part_one,
-        # part_two,
+        part_two,
     )
--- python/aoc2023/day14/parabolic_reflector_dish_test.py
+++ python/aoc2023/day14/parabolic_reflector_dish_test.py
@@ -29,14 +29,78 @@ PARSE_MODEL = (3, 2, {(0, 0): "O",
 MODEL = parse(EXAMPLE)
 
 
+def unparse(model):
+    w, h, rocks = model
+    sb = []
+    for y in range(h):
+        for x in range(w):
+            p = (x, y)
+            sb.append(rocks[p] if p in rocks else ".")
+        sb.append("\n")
+    return "".join(sb)
+
+
 def test_parse():
     assert parse(PARSE_EXAMPLE) == PARSE_MODEL
     assert unparse(PARSE_MODEL) == PARSE_EXAMPLE
+
+
+def test_example_two():
+    curr = MODEL
+    assert unparse(curr) == EXAMPLE
+    curr = spin_cycle(curr)
+    assert (
+        unparse(curr)
+        == """\
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+"""
+    )
+    curr = spin_cycle(curr)
+    assert (
+        unparse(curr)
+        == """\
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+"""
+    )
+    curr = spin_cycle(curr)
+    assert (
+        unparse(curr)
+        == """\
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+"""
+    )
 
 
 def test_part_one():
     assert part_one(MODEL) == 136
 
 
-# def test_part_two():
-#     assert part_two(MODEL) == 1_234
+def test_part_two():
+    assert part_two(MODEL) == 64
