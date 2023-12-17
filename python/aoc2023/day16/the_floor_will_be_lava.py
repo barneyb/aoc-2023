302ab82ba751696a2fb0b6a889626683663aa876
Barney Boisvert <bboisvert@gmail.com>
2023-12-16 16:26:04 -0800

    inline the methods on Dir; they're only used one place

--- python/aoc2023/day16/the_floor_will_be_lava.py
+++ python/aoc2023/day16/the_floor_will_be_lava.py
@@ -10,42 +10,6 @@ class Dir(Enum):
     SOUTH = 2
     WEST = 3
 
-    def slash(self, p):
-        x, y = p
-        match self:
-            case Dir.NORTH:
-                return (x + 1, y), Dir.EAST
-            case Dir.EAST:
-                return (x, y - 1), Dir.NORTH
-            case Dir.SOUTH:
-                return (x - 1, y), Dir.WEST
-            case Dir.WEST:
-                return (x, y + 1), Dir.SOUTH
-
-    def backslash(self, p):
-        x, y = p
-        match self:
-            case Dir.NORTH:
-                return (x - 1, y), Dir.WEST
-            case Dir.EAST:
-                return (x, y + 1), Dir.SOUTH
-            case Dir.SOUTH:
-                return (x + 1, y), Dir.EAST
-            case Dir.WEST:
-                return (x, y - 1), Dir.NORTH
-
-    def thru(self, p):
-        x, y = p
-        match self:
-            case Dir.NORTH:
-                return (x, y - 1), self
-            case Dir.EAST:
-                return (x + 1, y), self
-            case Dir.SOUTH:
-                return (x, y + 1), self
-            case Dir.WEST:
-                return (x - 1, y), self
-
 
 def parse(input):
     layout = {}
@@ -58,42 +22,60 @@ def parse(input):
 
 
 def count_energized(model, beam):
-    def in_bounds(p):
-        x, y = p
-        return 0 <= x < width and 0 <= y < height
+    def thru():
+        match heading:
+            case Dir.NORTH:
+                return (x, y - 1), heading
+            case Dir.EAST:
+                return (x + 1, y), heading
+            case Dir.SOUTH:
+                return (x, y + 1), heading
+            case Dir.WEST:
+                return (x - 1, y), heading
 
     width, height, layout = model
-    visited = set()
-    energized = set()
-    queue = deque()
-    queue.append(beam)
+    visited, energized = set(), set()
+    queue = deque([beam])
     while len(queue):
         curr = queue.pop()
         p, heading = curr
-        if not in_bounds(p):
-            continue
-        if curr in visited:
+        x, y = p
+        if curr in visited or not (0 <= x < width and 0 <= y < height):
             continue
         visited.add(curr)
         energized.add(p)
-        if p not in layout:
-            queue.append(heading.thru(p))
+        if p not in layout:  # an open square
+            queue.append(thru())
             continue
         match layout[p]:
             case "/":
-                queue.append(heading.slash(p))
+                match heading:
+                    case Dir.NORTH:
+                        queue.append(((x + 1, y), Dir.EAST))
+                    case Dir.EAST:
+                        queue.append(((x, y - 1), Dir.NORTH))
+                    case Dir.SOUTH:
+                        queue.append(((x - 1, y), Dir.WEST))
+                    case Dir.WEST:
+                        queue.append(((x, y + 1), Dir.SOUTH))
             case "\\":
-                queue.append(heading.backslash(p))
+                match heading:
+                    case Dir.NORTH:
+                        queue.append(((x - 1, y), Dir.WEST))
+                    case Dir.EAST:
+                        queue.append(((x, y + 1), Dir.SOUTH))
+                    case Dir.SOUTH:
+                        queue.append(((x + 1, y), Dir.EAST))
+                    case Dir.WEST:
+                        queue.append(((x, y - 1), Dir.NORTH))
             case "|" if heading in [Dir.NORTH, Dir.SOUTH]:  # through
-                queue.append(heading.thru(p))
+                queue.append(thru())
             case "|":  # split
-                x, y = p
                 queue.append(((x, y - 1), Dir.NORTH))
                 queue.append(((x, y + 1), Dir.SOUTH))
             case "-" if heading in [Dir.EAST, Dir.WEST]:  # through
-                queue.append(heading.thru(p))
+                queue.append(thru())
             case "-":  # split
-                x, y = p
                 queue.append(((x + 1, y), Dir.EAST))
                 queue.append(((x - 1, y), Dir.WEST))
     return len(energized)
@@ -104,20 +86,14 @@ def part_one(model):
 
 
 def part_two(model):
-    w, h, l = model
+    width, height, _ = model
     best = -1
-    for x in range(w):
-        best = max(
-            best,
-            count_energized(model, ((x, 0), Dir.SOUTH)),
-            count_energized(model, ((x, h - 1), Dir.NORTH)),
-        )
-    for y in range(h):
-        best = max(
-            best,
-            count_energized(model, ((0, y), Dir.EAST)),
-            count_energized(model, ((w - 1, y), Dir.WEST)),
-        )
+    for x in range(width):
+        best = max(best, count_energized(model, ((x, 0), Dir.SOUTH)))
+        best = max(best, count_energized(model, ((x, height - 1), Dir.NORTH)))
+    for y in range(height):
+        best = max(best, count_energized(model, ((0, y), Dir.EAST)))
+        best = max(best, count_energized(model, ((width - 1, y), Dir.WEST)))
     return best
 
 
