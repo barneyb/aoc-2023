d2806e3b200d0494874e75d1912925bd8a6f6c97
Barney Boisvert <bboisvert@gmail.com>
2023-12-17 11:35:20 -0800

    2023 day 17 part one

--- python/aoc2023/day17/clumsy_crucible.py
+++ python/aoc2023/day17/clumsy_crucible.py
@@ -1,5 +1,4 @@
 import math
-from collections import defaultdict
 from heapq import heappop, heappush
 
 from util import aoc
@@ -10,74 +9,57 @@ def parse(input):
     return len(lines[0]), len(lines), [[int(d) for d in l] for l in lines]
 
 
-def render(model, path):
-    width, height, layout = model
-    sb = []
-    for y, line in enumerate(layout):
-        for x, l in enumerate(line):
-            if (x, y) in path:
-                sb.append(path[(x, y)])
-            else:
-                sb.append(str(l))
-        sb.append("   ")
-        for x, l in enumerate(line):
-            if (x, y) in path:
-                sb.append(str(l))
-            else:
-                sb.append(".")
-        sb.append("\n")
-    return "".join(sb)
-
-
-def arrow(p1, p2):
-    (x1, y1), (x2, y2) = p1, p2
-    return "↑" if y1 > y2 else "←" if x1 > x2 else "↓" if y1 < y2 else "→"
-
-
 def part_one(model):
     def in_bounds(x, y):
         return 0 <= x < width and 0 <= y < height
 
-    def neighbors(p):
-        x, y = p
-        return [
-            (a, b)
-            for a, b in [(x, y - 1), (x + 1, y), (x, y + 1), (x - 1, y)]
-            if in_bounds(a, b)
-        ]
+    def move(pos, d):
+        x, y = pos
+        match d:
+            case 0:
+                return x, y - 1
+            case 1:
+                return x + 1, y
+            case 2:
+                return x, y + 1
+            case 3:
+                return x - 1, y
 
-    def heat_loss(p):
-        x, y = p
+    def heat_loss(x, y):
         return layout[y][x]
 
     width, height, layout = model
     start = (0, 0)
     goal = (width - 1, height - 1)
+    best = math.inf
     visited = set()
-    loss_at = defaultdict(lambda: math.inf)
-    loss_at[start] = 0  # heat loss starts as 0
-    prev = {}
-    pq = [(0, start)]
+    pq = [(0, start, ())]
     while len(pq):
-        hl, curr = heappop(pq)
-        visited.add(curr)
-        if hl >= loss_at[goal]:
+        hl, pos, path = heappop(pq)
+        if (pos, path) in visited:
             continue
-        for n in neighbors(curr):
-            if n not in visited:
-                alt = hl + heat_loss(n)
-                if alt < loss_at[n]:
-                    loss_at[n] = alt
-                    prev[n] = curr
-                    heappush(pq, (alt, n))
-    curr = goal
-    path = {}
-    while curr in prev:
-        p = prev[curr]
-        path[curr] = arrow(p, curr)
-        curr = p
-    print("\n" + render(model, path))
-    return loss_at[goal]
+        if hl >= best:
+            continue
+        if pos == goal:
+            best = hl
+            continue
+        visited.add((pos, path))
+        reverse = (path[-1] + 2) % 4 if len(path) else None
+        straight = None
+        if len(path) == 3:
+            a, b, c = path
+            if a == b == c:
+                straight = a
+        for d in range(4):
+            if d == reverse or d == straight:
+                continue
+            p = move(pos, d)
+            if in_bounds(*p):
+                np = path[-2:] + (d,)
+                if (p, np) not in visited:
+                    heappush(pq, (hl + heat_loss(*p), p, np))
+
+    return best
 
 
 # def part_two(model):
