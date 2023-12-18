55e1143c3641f007331373b9124561d7b8579bac
Barney Boisvert <bboisvert@gmail.com>
2023-12-18 07:29:45 -0800

    2023 day 18 part one

--- python/aoc2023/day18/lavaduct_lagoon.py
+++ python/aoc2023/day18/lavaduct_lagoon.py
@@ -40,18 +40,57 @@ def cut_trench(plan):
         for _ in range(n):
             curr = move(curr, d)
             trench[curr] = c
+    return trench
+
+
+def bounds(trench):
     x1, y1 = 0, 0
     x2, y2 = 0, 0
     for x, y in trench:
         x1, y1 = min(x1, x), min(y1, y)
         x2, y2 = max(x2, x), max(y2, y)
-    return (x1, y1), (x2, y2), trench
+    return (x1, y1), (x2, y2)
+
+
+def find_point_inside(lo, hi, trench):
+    y = (lo[1] + hi[1]) // 2
+    inside = False
+    x = lo[0]
+    while x <= hi[0]:
+        if not inside and (x, y) in trench:
+            inside = True
+        if inside and (x, y) not in trench:
+            break
+        x += 1
+    return x, y
+
+
+def dig_out_lagoon(trench, start):
+    def enqueue(p):
+        if p not in lagoon and p not in trench:
+            queue.append(p)
+
+    lagoon = set()
+    queue = [start]
+    while len(queue):
+        p = queue.pop()
+        if p in lagoon:
+            continue
+        lagoon.add(p)
+        x, y = p
+        enqueue((x, y - 1))
+        enqueue((x + 1, y))
+        enqueue((x, y + 1))
+        enqueue((x - 1, y))
+    return lagoon
 
 
 def part_one(plan):
-    lo, hi, trench = cut_trench(plan)
-    print(f"{lo} -> {hi}")
-    return len(trench)
+    trench = cut_trench(plan)
+    lo, hi = bounds(trench)
+    p = find_point_inside(lo, hi, trench)
+    lagoon = dig_out_lagoon(trench, p)
+    return len(trench) + len(lagoon)
 
 
 # def part_two(model):
--- python/aoc2023/day18/lavaduct_lagoon_viz.py
+++ python/aoc2023/day18/lavaduct_lagoon_viz.py
@@ -1,15 +1,19 @@
 from PIL import Image, ImageDraw
 
-import aoc2023.day18 as lagoon
+import aoc2023.day18 as ll
 from util import aoc
 
 PX = None
 GROUND = "#333333"
+LAGOON = "#FF8000"
 
 if __name__ == "__main__":
     data = aoc.get_input(__file__)
-    plan = lagoon.parse(data)
-    lo, hi, trench = lagoon.cut_trench(plan)
+    plan = ll.parse(data)
+    trench = ll.cut_trench(plan)
+    lo, hi = ll.bounds(trench)
+    inside = ll.find_point_inside(lo, hi, trench)
+    lagoon = ll.dig_out_lagoon(trench, inside)
     dx, dy = lo
     dx, dy = -dx, -dy
     width = hi[0] - lo[0] + 1
@@ -17,6 +21,9 @@ if __name__ == "__main__":
 
     im = Image.new(mode="RGB", size=(width, height), color=GROUND)
     draw = ImageDraw.Draw(im)
+    for x, y in lagoon:
+        x, y = x + dx, y + dy
+        draw.rectangle((x, y, x, y), LAGOON)
     prev = (dx, dy)
     for (x, y), c in trench.items():
         p = (x + dx, y + dy)
