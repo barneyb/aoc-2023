b2b467669cc603f86d17e6d64756c5bfa67e45f8
Barney Boisvert <bboisvert@gmail.com>
2023-12-17 20:53:39 -0800

    clean up the Graph constructor/parser a bit

--- python/aoc2023/day17/clumsy_graph.py
+++ python/aoc2023/day17/clumsy_graph.py
@@ -10,36 +10,34 @@ class NotEnoughRoom(RuntimeError):
 
 class Graph:
     def __init__(self, input):
+        def add_edges(i, j, d):
+            ic, ia = pairs[i]
+            jc, ja = pairs[j]
+            ia.append((j, jc, d))
+            ja.append((i, ic, (d + 2) % 4))
+
         lines = input.splitlines()
         height = len(lines)
         width = len(lines[0])
-        self.start = 0
-        self.goal = width * height - 1
-        self.nodes = [
-            (int(c), []) for y, line in enumerate(lines) for x, c in enumerate(line)
-        ]
-
-        def add_edges(i, j, d):
-            ic, ia = self.nodes[i]
-            jc, ja = self.nodes[j]
-            ia.append((j, jc, d))
-            ja.append((i, ic, (d + 2) % 4))
-            pass
-
+        pairs = [(int(c), []) for line in lines for c in line]
         for y in range(height):
             for x in range(width):
                 i = y * width + x
                 if y > 0:
-                    add_edges(i, (y - 1) * width + x, 0)
+                    add_edges(i, i - width, 0)
                 if x > 0:
-                    add_edges(i, y * width + x - 1, 3)
+                    add_edges(i, i - 1, 3)
 
-    def edges(self, n):
-        return self.nodes[n][1]
+        self.start = 0
+        self.goal = width * height - 1
+        self.nodes = [es for _, es in pairs]
+
+    def __getitem__(self, n):
+        return self.nodes[n]
 
 
-def either_part(G, allowed_moves):
-    pq = [(0, G.start, ())]
+def either_part(graph, allowed_moves):
+    pq = [(0, graph.start, ())]
     visited = set()
     best = math.inf
     while len(pq):
@@ -47,7 +45,7 @@ def either_part(G, allowed_moves):
         v = (pos, path)
         if loss >= best or v in visited:
             continue
-        if pos == G.goal:
+        if pos == graph.goal:
             best = loss
             continue
         visited.add(v)
@@ -57,24 +55,24 @@ def either_part(G, allowed_moves):
     return best
 
 
-def part_one(G):
+def part_one(graph):
     def allowed_moves(pos, path):
-        for n, l, d in G.edges(pos):
+        for n, l, d in graph[pos]:
             if len(path) and (d + 2) % 4 == path[-1]:
                 continue  # no reverse
             if len(path) == 3 and all(p == d for p in path):
                 continue  # no four in a row
             yield n, l, path[-2:] + (d,)
 
-    return either_part(G, allowed_moves)
+    return either_part(graph, allowed_moves)
 
 
-def part_two(G):
+def part_two(graph):
     def extend(n, l, np):
         d = np[-1]
         while any(p != d for p in np[-4:]):
             found = False
-            for a, b, c in G.edges(n):
+            for a, b, c in graph[n]:
                 if d == c:
                     found = True
                     n = a
@@ -86,11 +84,11 @@ def part_two(G):
         return n, l, np
 
     def allowed_moves(pos, path):
-        for n, l, d in G.edges(pos):
+        for n, l, d in graph[pos]:
             heading = path[-1] if len(path) else d
             if (d + 2) % 4 == heading:
                 continue  # no reverse
-            if n == G.goal:
+            if n == graph.goal:
                 if d != heading or any(p != heading for p in path[-3:]):
                     continue
             if len(path) == 10 and d == heading:
@@ -101,7 +99,7 @@ def part_two(G):
             except NotEnoughRoom:
                 pass
 
-    return either_part(G, allowed_moves)
+    return either_part(graph, allowed_moves)
 
 
 if __name__ == "__main__":
