c23823252479b403d3178b4290ace3808b1eee2a
Barney Boisvert <bboisvert@gmail.com>
2023-12-25 11:23:38 -0800

    much less wasteful. still far too wasteful.

--- python/aoc2023/day21/step_counter_test.py
+++ python/aoc2023/day21/step_counter_test.py
@@ -17,9 +17,37 @@ EXAMPLE = """\
 GARDEN = Garden(EXAMPLE)
 
 
+def test_unparse():
+    assert str(GARDEN) == EXAMPLE
+
+
 def test_part_one():
     assert part_one(GARDEN, 6) == 16
 
 
-# def test_part_two():
-#     assert part_two(GARDEN) == 1_234
+def test_part_two_6():
+    assert part_two(GARDEN, 6) == 16
+
+
+def test_part_two_10():
+    assert part_two(GARDEN, 10) == 50
+
+
+def test_part_two_50():
+    assert part_two(GARDEN, 50) == 1594
+
+
+def test_part_two_100():
+    assert part_two(GARDEN, 100) == 6536
+
+
+def test_part_two_500():
+    assert part_two(GARDEN, 500) == 167004
+
+
+# def test_part_two_1000():
+#     assert part_two(GARDEN, 1000) == 668697
+
+
+# def test_part_two_5000():
+#     assert part_two(GARDEN, 5000) == 16733044
