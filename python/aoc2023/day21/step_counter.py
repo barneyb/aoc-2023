58ef3c6fdb647689a711a031b153ec075bfa12d0
Barney Boisvert <bboisvert@gmail.com>
2024-02-04 13:33:57 -0800

    do the _garden_ counting. still need to count plots

--- python/aoc2023/day21/step_counter.py
+++ python/aoc2023/day21/step_counter.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from util import aoc
 
 
@@ -24,22 +26,94 @@ def parse(input):
             up = n - width
             if graph[up] is not None:
                 edge(up, n)
-    return start, graph
+    return start, graph, width
 
 
 def part_one(model, steps=64):
-    start, graph = model
+    start, graph, width = model
     reached = {start}
     for _ in range(steps):
-        prev = reached
-        reached = set()
-        for n in prev:
-            reached.update(graph[n])
+        reached = step(graph, reached)
     return len(reached)
 
 
-# def part_two(model):
-#     return None
+def step(graph, reached):
+    prev = reached
+    reached = set()
+    for n in prev:
+        reached.update(graph[n])
+    return reached
+
+
+def get_edges(width):
+    half = width // 2
+    center = half * width + half
+    north = half
+    east = center + half
+    south = half + width * (width - 1)
+    west = center - half
+    return north, east, south, west
+
+
+def get_corners(width):
+    nw = 0
+    ne = width - 1
+    sw = width * (width - 1)
+    se = width * width - 1
+    return nw, ne, se, sw
+
+
+def part_two(model, steps=26_501_365):
+    start, graph, width = model
+    half = width // 2
+    # assert (steps - half) % width == 0
+    full_rounds = steps // width
+    # after the initial half round and N full rounds:
+    # 1 center tile, at 1.5 rounds
+    # N-1 each cardinal tile, at 2 rounds
+    # 1 each cardinal tile, at 1 round
+    # triangle(N-2) each quadrant tile, at 2 rounds
+    # N-1 each quadrant tile, at 1.5 rounds
+    # N each quadrant tile, at .5 rounds
+
+    # reached after 65 steps
+    north, east, south, west = get_edges(width)
+    # reached after 130 steps
+    nw, ne, se, sw = get_corners(width)
+    reached = {start}
+    firsts = defaultdict(lambda: None)
+    for s in range(width * 2):
+        for n in [start, north, east, south, west, nw, ne, se, sw]:
+            if n not in firsts and n in reached:
+                # noinspection PyTypeChecker
+                firsts[n] = s
+        reached = step(graph, reached)
+    for n in [north, east, south, west, nw, ne, se, sw]:
+        print(f"reached {n} in {firsts[n]}")
+
+
+def draw(model, reached, tl, br):
+    start, graph, width = model
+    x1, y1 = tl
+    x2, y2 = br
+    sb = []
+    for y in range(y1, y2 + 1):
+        for x in range(x1, x2 + 1):
+            n = y * width + x
+            if x < 0 or x >= width:
+                sb.append("-")
+            elif y < 0 or y >= width:
+                sb.append("|")
+            elif n in reached:
+                sb.append("O")
+            elif n == start:
+                sb.append("S")
+            elif graph[n] is None:
+                sb.append("#")
+            else:
+                sb.append(".")
+        sb.append("\n")
+    print("".join(sb))
 
 
 if __name__ == "__main__":
@@ -47,5 +121,5 @@ if __name__ == "__main__":
         __file__,
         parse,
         part_one,
-        # part_two,
+        part_two,
     )
--- python/aoc2023/day21/step_counter_test.py
+++ python/aoc2023/day21/step_counter_test.py
@@ -14,16 +14,69 @@ EXAMPLE = """\
 ...........
 """
 
+EXAMPLE_TWO = """\
+...........
+..#...##.#.
+.###..#..#.
+..#.#...#..
+....#.#....
+.....S.....
+.##......#.
+....#..##..
+.##.#.####.
+.##...#.##.
+...........
+"""
+
+MODEL = parse(EXAMPLE)
+MODEL_TWO = parse(EXAMPLE_TWO)
+
 
 def test_parse():
-    start, graph = parse(EXAMPLE)
+    start, graph, width = MODEL
     assert start == 60
     assert graph[start] == [59, 49]
+    assert width == 11
 
 
 def test_part_one():
-    assert part_one(parse(EXAMPLE), 6) == 16
+    assert part_one(MODEL, 6) == 16
 
 
-# def test_part_two():
-#     assert part_two(MODEL) == 1_234
+def test_get_edges():
+    north, east, south, west = get_edges(11)
+    assert north == 5
+    assert east == 65
+    assert south == 115
+    assert west == 55
+
+
+def test_get_corners():
+    nw, ne, se, sw = get_corners(11)
+    assert nw == 0
+    assert ne == 10
+    assert se == 120
+    assert sw == 110
+
+
+def test_part_two_6():
+    assert part_two(MODEL_TWO, 6) == 36
+
+
+def test_part_two_10():
+    assert part_two(MODEL_TWO, 10) == 88
+
+
+def test_part_two_50():
+    assert part_two(MODEL_TWO, 50) == 1904
+
+
+def test_part_two_100():
+    print()
+    assert part_two(MODEL_TWO, 100) == 7465
+    assert part_two(MODEL_TWO, 101) == 7594
+
+
+def test_part_two_200():
+    print()
+    assert part_two(MODEL_TWO, 200) == 29599
