275e9dccb068a59e8a26c37e329d3ef069525f09
Barney Boisvert <bboisvert@gmail.com>
2024-06-29 10:26:17 -0700

    rather simpler; rather faster

--- src/main/java/com/barneyb/aoc/aoc2023/day24/NeverTellMeTheOdds.java
+++ src/main/java/com/barneyb/aoc/aoc2023/day24/NeverTellMeTheOdds.java
@@ -7,13 +7,17 @@ import com.barneyb.aoc.util.SolveEachPart;
 import com.google.ortools.Loader;
 import com.google.ortools.sat.CpModel;
 import com.google.ortools.sat.CpSolver;
+import com.google.ortools.sat.CpSolverSolutionCallback;
 import com.google.ortools.sat.CpSolverStatus;
 import com.google.ortools.sat.IntVar;
-import com.google.ortools.sat.LinearArgument;
 import com.google.ortools.sat.LinearExpr;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class NeverTellMeTheOdds extends SolveEachPart<List<Hailstone>, Long, Long> {
 
@@ -30,66 +34,117 @@ public class NeverTellMeTheOdds extends SolveEachPart<List<Hailstone>, Long, Lon
         return solvePartOne(hailstones, 200_000_000_000_000L, 400_000_000_000_000L);
     }
 
-    private int count = 0;
-    private final long lo = Long.MIN_VALUE >> 14;
-    private final long hi = Long.MAX_VALUE >> 14;
+    private static class Storm {
 
-    private String name(String prefix) {
-        return prefix + count++;
-    }
+        private final long lo, hi;
+        private final Map<String, Integer> counts = new HashMap<>();
+        final List<IntVar> ofNote;
+        final IntVar x, y, z, a, b, c;
 
-    private void doit(CpModel model, IntVar p, IntVar v, IntVar t, long pos, long vel) {
-        // i = -v
-        var i = model.newIntVar(lo, hi, name("i"));
-        model.addMultiplicationEquality(i, v, LinearExpr.constant(-1));
-        // j = -2 + i
-        var j = model.newIntVar(lo, hi, name("j"));
-        model.addEquality(j, LinearExpr.sum(new LinearArgument[]{ LinearExpr.constant(vel), i }));
-        // k = j * t
-        var k = model.newIntVar(lo, hi, name("k"));
-        model.addMultiplicationEquality(k, j, t);
-        // p = k + 19
-        model.addEquality(p, LinearExpr.sum(new LinearArgument[]{ k, LinearExpr.constant(pos) }));
-    }
+        Storm(List<Hailstone> hailstones, CpModel model) {
+            long lo = Long.MAX_VALUE;
+            long hi = Long.MIN_VALUE;
+            for (var h : hailstones) {
+                var x = h.pos().x();
+                var y = h.pos().y();
+                var z = h.pos().z();
+                lo = Math.min(lo, Math.min(x, Math.min(y, z)));
+                hi = Math.max(hi, Math.max(x, Math.max(y, z)));
+            }
+            this.lo = lo = Math.min(Long.MIN_VALUE >> 13, lo);
+            this.hi = hi = Math.min(Long.MAX_VALUE >> 13, hi);
 
-    @Override
-    protected Long solvePartTwo(List<Hailstone> hailstones) {
-        var model = new CpModel();
-        var ts = new ArrayList<IntVar>(hailstones.size());
-        var x = model.newIntVar(lo, hi, "x");
-        var y = model.newIntVar(lo, hi, "y");
-        var z = model.newIntVar(lo, hi, "z");
-        var a = model.newIntVar(lo, hi, "a");
-        var b = model.newIntVar(lo, hi, "b");
-        var c = model.newIntVar(lo, hi, "c");
-        for (int i = 0; i < hailstones.size(); i++) {
-            var h = hailstones.get(i);
-            var t = model.newIntVar(0, hi, "t" + i);
-            ts.add(t);
-            doit(model, x, a, t, h.pos().x(), h.vel().x());
-            doit(model, y, b, t, h.pos().y(), h.vel().y());
-            doit(model, z, c, t, h.pos().z(), h.vel().z());
+            ofNote = new ArrayList<>(hailstones.size() + 6);
+            ofNote.add(x = model.newIntVar(lo, hi, "x"));
+            ofNote.add(y = model.newIntVar(lo, hi, "y"));
+            ofNote.add(z = model.newIntVar(lo, hi, "z"));
+            ofNote.add(a = model.newIntVar(lo, hi, "a"));
+            ofNote.add(b = model.newIntVar(lo, hi, "b"));
+            ofNote.add(c = model.newIntVar(lo, hi, "c"));
+            for (Hailstone h : hailstones) {
+                var t = model.newIntVar(0, hi - lo, name("t"));
+                ofNote.add(t);
+                addStoneDimension(model, x, a, t, h.pos().x(), h.vel().x());
+                addStoneDimension(model, y, b, t, h.pos().y(), h.vel().y());
+                addStoneDimension(model, z, c, t, h.pos().z(), h.vel().z());
+            }
+            ofNote.forEach(v -> model.addHint(v, 0));
         }
-        CpSolver solver = new CpSolver();
-        CpSolverStatus status = solver.solve(model);
-        if (status == CpSolverStatus.OPTIMAL || status == CpSolverStatus.FEASIBLE) {
-            System.out.printf("[%3.0f ms]", solver.wallTime() * 1000);
-            System.out.print(" x = " + solver.value(x));
-            System.out.print(", y = " + solver.value(y));
-            System.out.print(", z = " + solver.value(z));
-            System.out.print(", a = " + solver.value(a));
-            System.out.print(", b = " + solver.value(b));
-            System.out.print(", c = " + solver.value(c));
-            for (var t : ts) {
-                System.out.printf(", %s = %d", t.getName(), solver.value(t));
+
+        private String name(String prefix) {
+            return prefix + counts.compute(prefix,
+                                           (p, i) -> i == null
+                                                   ? 0
+                                                   : (i + 1));
+        }
+
+        private void addStoneDimension(CpModel model, IntVar p, IntVar v, IntVar t, long pos, long vel) {
+            // p = (vel + -v) * t + pos
+            // i = -v
+            var i = model.newIntVar(lo, hi, name("i"));
+            model.addEquality(i, LinearExpr.term(v, -1));
+            // j = vel + i
+            var j = model.newIntVar(lo, hi, name("j"));
+            model.addEquality(j, LinearExpr.affine(i, 1, vel));
+            // k = j * t
+            var k = model.newIntVar(lo, hi, name("k"));
+            model.addMultiplicationEquality(k, j, t);
+            // p = k + pos
+            model.addEquality(p, LinearExpr.affine(k, 1, pos));
+        }
+
+    }
+
+    private static class SolutionCallback extends CpSolverSolutionCallback {
+
+        private final Storm storm;
+        private final Set<Long> solutions = new HashSet<>();
+
+        public Set<Long> getSolutions() {
+            return solutions;
+        }
+
+        SolutionCallback(Storm storm) {
+            this.storm = storm;
+        }
+
+        @Override
+        public synchronized void onSolutionCallback() {
+            long s = value(storm.x)
+                     + value(storm.y)
+                     + value(storm.z);
+            if (!solutions.add(s)) {
+                return;
+            }
+            System.out.println(solutions.size() == 1
+                                       ? "Found solution!"
+                                       : "Found another solution!");
+            System.out.printf("[%3.0f ms]", wallTime() * 1000);
+            System.out.print(" x = " + value(storm.x));
+            System.out.print(", y = " + value(storm.y));
+            System.out.print(", z = " + value(storm.z));
+            System.out.print(", a = " + value(storm.a));
+            System.out.print(", b = " + value(storm.b));
+            System.out.print(", c = " + value(storm.c));
+            for (var t : storm.ofNote) {
+                System.out.printf(", %s = %d", t.getName(), value(t));
             }
             System.out.println();
-            return solver.value(x)
-                   + solver.value(y)
-                   + solver.value(z);
-        } else {
+        }
+
+    }
+
+    protected Long solvePartTwo(List<Hailstone> hailstones) {
+        var model = new CpModel();
+        var storm = new Storm(hailstones, model);
+        CpSolver solver = new CpSolver();
+//        solver.getParameters().setEnumerateAllSolutions(true);
+        SolutionCallback cb = new SolutionCallback(storm);
+        CpSolverStatus status = solver.solve(model, cb);
+        if (cb.getSolutions().isEmpty()) {
             throw new RuntimeException("No solution found: " + status);
         }
+        return cb.getSolutions().iterator().next();
     }
 
     @Override
